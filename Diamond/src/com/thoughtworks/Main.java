a7d657f6b9e3cf1515c4c4bd90ba8d96a1edab6d
CooTWer <zryu@thoughtworks.com>
2016-09-13 11:27:18 +0800

    sixth work commit

---
+++ Diamond/src/com/thoughtworks/Main.java
@@ -0,0 +1,34 @@
+package com.thoughtworks;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String args[]){
+        Stack<String> lineStack = new Stack<String>();
+        int round = Integer.parseInt(args[0]);
+        int startPosition;
+        int starNumber;
+        String tempString = "";
+        for (int i=0; i<round; i++){
+            startPosition = round - i - 1;
+            starNumber = i*2+1;
+            while (startPosition!=0){
+                System.out.print(' ');
+                tempString += ' ';
+                startPosition--;
+            }
+            while (starNumber!=0){
+                System.out.print('*');
+                tempString += '*';
+                starNumber--;
+            }
+            lineStack.push(tempString);
+            System.out.print('\n');
+            tempString = "";
+        }
+        lineStack.pop();
+        while (!lineStack.empty()){
+            System.out.println(lineStack.pop());
+        }
+    }
+}
