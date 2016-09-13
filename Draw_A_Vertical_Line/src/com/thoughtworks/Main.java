637de8890d03011ad9a572862167e7418e9cad90
CooTWer <zryu@thoughtworks.com>
2016-09-13 10:42:24 +0800

    third work commit

---
+++ Draw_A_Vertical_Line/src/com/thoughtworks/Main.java
@@ -0,0 +1,10 @@
+package com.thoughtworks;
+
+public class Main {
+    public static void main(String args[]){
+        int round = Integer.parseInt(args[0]);
+        for(int i=0; i<round; i++){
+            System.out.println("*");
+        }
+    }
+}
